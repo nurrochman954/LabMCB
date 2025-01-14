7eb36e216249872fa4b2323e58465c6e7e39841f
danellazs <danella.zefanya@gmail.com>
2025-01-14 11:11:37 +0700

    Add : Panduan boxes and download button component

--- labmcb/components/Panduan.tsx
+++ labmcb/components/Panduan.tsx
@@ -1,9 +1,107 @@
 import React from 'react';
+import UnduhSurat from "./UnduhSuratButton";
 
 const BoxPanduan = () => {
-    return (
-        <div className="w-screen h-[100px] bg-gray-200"></div>
-    );
+  return (
+    <>
+      <div
+        className="w-full h-[400px] shadow-md gradient-box flex items-center"
+        style={{
+          margin: '10px auto',
+          padding: '20px 20px 20px 0',
+          color: '#000',
+          fontFamily: 'Lato, sans-serif',
+          boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.25)',
+          marginBottom: '30px',
+        }}
+      >
+        <div
+          className="w-[250px] h-[250px] bg-[#FED9B7] flex items-center justify-center"
+          style={{
+            borderTopRightRadius: '150px',
+            borderBottomRightRadius: '150px',
+            borderTopLeftRadius: '0',
+            borderBottomLeftRadius: '0',
+            fontWeight: 'bold'
+          }}
+        >
+          <p className="text-xl text-center">
+            Panduan Analisis Permohonan
+          </p>
+        </div>
+
+        <div
+          className="flex items-center justify-center"
+          style={{
+            flexDirection: 'column',
+            height: '100%',
+          }}
+        >
+          <div className="flex-1 p-10 text-sm pt-5 pb-5">
+            <ol className="list-decimal space-y-1">
+              <li>Customer mengunduh File Surat Pengantar</li>
+              <li>Customer mengisi Formulir Permohonan Analisis</li>
+              <li>Customer mengirimkan sampel kepada Admin Laboratorium. Pengiriman sampel bisa secara langsung datang ke Balai Konservasi Borobudur atau diantar dengan jasa ekspedisi</li>
+              <li>Untuk memantau proses analisis/uji sampel customer dapat mengecek secara berkala pada page “Halaman Saya” dan fitur “Tracking Permohonan Analisis dan Penyewaan Alat”</li>
+              <li>Admin Laboratorium akan memberikan jawaban apakah Supervisor menyetujui atau tidak pada proses analisis/uji sampel</li>
+              <li>Apabila proses Analisis/uji sampel telah di selesaikan oleh Laboran, customer akan mendapatkan invoice/faktur untuk segera melunaskan pembayaran dan mengunggah bukti pembayaran.</li>
+              <li>Setelah bukti pembayaran diterima, Admin Laboratorium akan mengirimkan Laporan Analisis/Uji sampel</li>
+              <li>Untuk informasi lebih lanjut, dapat menghubungi nomor dibawah ini</li>
+            </ol>
+          </div>
+          <div className="flex justify-end w-full">
+            <UnduhSurat />
+          </div>
+        </div>
+      </div>
+
+      <div
+        className="w-full h-[300px] shadow-md gradient-box flex items-center"
+        style={{
+          margin: '10px auto',
+          padding: '20px 20px 20px 0',
+          color: '#000',
+          fontFamily: 'Lato, sans-serif',
+          boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.25)',
+          marginBottom: '30px',
+        }}
+      >
+        <div
+          className="w-[250px] h-[250px] bg-[#FED9B7] flex items-center justify-center"
+          style={{
+            borderTopRightRadius: '150px',
+            borderBottomRightRadius: '150px',
+            borderTopLeftRadius: '0',
+            borderBottomLeftRadius: '0',
+            fontWeight: 'bold'
+          }}
+        >
+          <p className="text-xl text-center">
+            Panduan Penyewaan Alat
+          </p>
+        </div>
+
+        <div
+          className="flex items-center justify-center p-10 text-sm pt-5 pb-5"
+          style={{
+            flexDirection: 'column',
+            height: '100%',
+          }}
+        >
+            <ol className="list-decimal space-y-1">
+              <li>Customer mengunduh File Surat Pengantar</li>
+              <li>Customer mengisi Formulir Permohonan Analisis</li>
+              <li>Customer mengirimkan sampel kepada Admin Laboratorium. Pengiriman sampel bisa secara langsung datang ke Balai Konservasi Borobudur atau diantar dengan jasa ekspedisi</li>
+              <li>Untuk memantau proses analisis/uji sampel customer dapat mengecek secara berkala pada page “Halaman Saya” dan fitur “Tracking Permohonan Analisis dan Penyewaan Alat”</li>
+              <li>Admin Laboratorium akan memberikan jawaban apakah Supervisor menyetujui atau tidak pada proses analisis/uji sampel</li>
+              <li>Apabila proses Analisis/uji sampel telah di selesaikan oleh Laboran, customer akan mendapatkan invoice/faktur untuk segera melunaskan pembayaran dan mengunggah bukti pembayaran.</li>
+              <li>Setelah bukti pembayaran diterima, Admin Laboratorium akan mengirimkan Laporan Analisis/Uji sampel</li>
+              <li>Untuk informasi lebih lanjut, dapat menghubungi nomor dibawah ini</li>
+            </ol>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default BoxPanduan;
---
+++ labmcb/components/UnduhSuratButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UnduhSurat = () => {
+    return (
+        <button className="bg-primary text-white px-6 py-3 rounded-md text-lg font-semibold hover:bg-[#14244d]">
+            <img src="/assets/Download.png" alt="Download" className="inline-block w-5 h-5 mr-2" />
+            Unduh File Surat Pengantar
+        </button>
+    );
+};
+
+export default UnduhSurat;
