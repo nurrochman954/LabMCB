f9d9e577a9fba5d7ed87c5d21925374c283c5413
athayaputriii <athayputri100@gmail.com>
2025-01-14 10:34:49 +0700

    Tracking

--- labmcb/app/(root)/halaman-saya/page.tsx
+++ labmcb/app/(root)/halaman-saya/page.tsx
@@ -3,16 +3,25 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import TopBar from "@/components/TopBar";
 import ProfileBox from "@/components/ProfileBox";
+import SmallTracking from "@/components/SmallTracking";
+import Tracking from "@/components/Tracking";
 
-const HalamanSaya = () => {
+const HalamanSaya : React.FC = () =>{
     return (
+      
       <>
         <Header />
         <TopBar />
         <ProfileBox 
         text= "Athaya Putri Harmana"
         imageUrl="https://cdn0-production-images-kly.akamaized.net/zmTJIzsJi7IfBJCh0uBylr9u1jU=/1200x900/smart/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/5050791/original/062807400_1734171357-shin-tae-yong_26cccc2.jpg"/>
+        
+        
+        <SmallTracking/>
+        <Tracking/>
         <Footer />
+
+
       </>
     );
   };
---
+++ labmcb/components/SmallTracking.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const NomorPermohonan = () => (
+  <div style={{
+    backgroundColor: '#FAEBD7', // Warna krem
+    padding: '20px',
+    borderRadius: '10px',
+    border: '1px solid #ccc',
+    width: '1208px',
+    height : '74px', // Anda dapat menyesuaikan lebar sesuai kebutuhan
+    marginLeft: '100px',
+     // Centering the box
+  }}>
+    <h2 style={{
+      fontWeight: 'bold',
+      fontSize: '24px',
+      textAlign: 'left',
+      margin: 0,
+    }}>
+      Nomor Permohonan
+    </h2>
+  </div>
+);
+
+export default NomorPermohonan;
