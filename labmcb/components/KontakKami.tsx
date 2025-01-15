89a5a12d465b39c34df45d4e8ced898edf1b420d
athayaputriii <athayputri100@gmail.com>
2025-01-15 15:36:15 +0700

    fixing kontak Kami

--- labmcb/components/KontakKami.tsx
+++ labmcb/components/KontakKami.tsx
@@ -1,15 +1,30 @@
-import React from 'react';
+"use client";
+import React, { useState } from "react";
 
 const KontakKami: React.FC = () => {
+  const [isNextHovered, setIsNextHovered] = useState(false);
   return (
     <a 
-      href="https://wa.me/+6281931747582" 
+      href="https://wa.me/6281931747582"  // Format yang benar tanpa tanda +
       className="floating-button" 
       target="_blank" 
       rel="noopener noreferrer"
     >
-      <span className="icon">✆</span>
-      Kontak Kami
+       <button
+          onMouseEnter={() => setIsNextHovered(true)}
+          onMouseLeave={() => setIsNextHovered(false)}
+          className="p-2"
+>
+      <img
+        src={isNextHovered ?   "/assets/KontakKamiColor.png" : "/assets/KontakKami.png"}
+        alt="Right Arrow"
+        className="w-4 h-4 transition-all duration-300 ease-in-out"
+        style={{
+          width: '160px', // Sesuaikan ukuran gambar
+          height: 'auto', // Sesuaikan ukuran gambar
+        }}
+      />
+</button>
     </a>
   );
 };
