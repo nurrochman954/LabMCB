13cbeffc8384e49cff534eee33fae28b0d2093d7
Ozwald-koaltz <kenzotaro9999@gmail.com>
2025-01-15 14:44:08 +0700

    Update : Component LabProfile.tsx

--- labmcb/components/LabProfile.tsx
+++ labmcb/components/LabProfile.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
-interface LabProfile {
+interface LabProfileProps {
   imageUrl: string;
   text: string;
 }
 
-const LabProfile: React.FC<LabProfile> = ({ imageUrl, text }) => {
+const LabProfile: React.FC<LabProfileProps> = ({ imageUrl, text }) => {
   const LabProfileStyle: React.CSSProperties = {
-    maxWidth: '600px',
+    display: 'flex',
+    maxWidth: '1000px',
+    height: 'auto',
     margin: '10px auto',
     background: 'radial-gradient(farthest-corner at 80% 80%, #FED9B7 0%, #F8AD96 45%, #F7A38E 57%, #F59986 63%, #F17C6F 86%, #F07167 100%)',
     borderRadius: '12px',
@@ -15,31 +17,32 @@ const LabProfile: React.FC<LabProfile> = ({ imageUrl, text }) => {
     padding: '20px',
     color: '#000',
     fontFamily: 'Lato, sans-serif',
+    flexDirection: 'row', // Set the flex direction to row
+    alignItems: 'center',
   };
 
   const imageStyle: React.CSSProperties = {
-    width: '100%',
+    width: '40%', // Adjust the width to take up less space
     borderRadius: '12px',
-    marginBottom: '20px',
+    marginLeft: '40px',
     objectFit: 'cover',
-    maxWidth: '480px',
+    maxWidth: '480px', // Adjust max dimensions to fit the new layout
     maxHeight: '280px',
-    display: 'block',
-    marginLeft: 'auto',
-    marginRight: 'auto' 
   };
 
   const textStyle: React.CSSProperties = {
     textAlign: 'justify',
     lineHeight: '1.4',
-    marginTop: '20px',
-    marginLeft: '20px',
-    marginRight: '20px'
+    marginLeft: '40px',
+    marginRight: '30px',
+    marginBottom: '30px',
+    marginTop: '30px',
+    flex: '1',
   };
 
   return (
     <div style={LabProfileStyle}>
-      <img src={imageUrl} alt="LabProfile" style={imageStyle} />
+      <img src={imageUrl} alt="Lab Profile" style={imageStyle} />
       <p style={textStyle}>{text}</p>
     </div>
   );
