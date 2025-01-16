5b83a640b6bba2202c336a0ffb8d562db4b4304f
Nur Rochman <nurrochman954@gmail.com>
2025-01-16 15:04:04 +0700

    Update middleware

--- labmcb/middleware.ts
+++ labmcb/middleware.ts
@@ -2,10 +2,19 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 import { NextResponse } from 'next/server'
 
 const isAdminRoute = createRouteMatcher(['/admin(.*)'])
+const isUserRoute = createRouteMatcher(['/(root)?(.*)'])  // Mencakup root routes dan (root) group
 
 export default clerkMiddleware(async (auth, req) => {
-  // Protect all routes starting with `/admin`
-  if (isAdminRoute(req) && (await auth()).sessionClaims?.metadata?.role !== 'admin') {
+  const role = (await auth()).sessionClaims?.metadata?.role;
+  
+  // Jika admin mencoba akses route non-admin
+  if (role === 'admin' && !isAdminRoute(req)) {
+    const url = new URL('/admin', req.url)
+    return NextResponse.redirect(url)
+  }
+
+  // Jika user biasa mencoba akses route admin
+  if (role !== 'admin' && isAdminRoute(req)) {
     const url = new URL('/', req.url)
     return NextResponse.redirect(url)
   }
@@ -13,9 +22,7 @@ export default clerkMiddleware(async (auth, req) => {
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
     '/(api|trpc)(.*)',
   ],
 }
