717c81ce8d7aa7f7b49f11c99b013425a1d7f638
Nur Rochman <nurrochman954@gmail.com>
2025-01-20 08:13:59 +0700

    Update next.config

--- labmcb/next.config.ts
+++ labmcb/next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: '/api/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
