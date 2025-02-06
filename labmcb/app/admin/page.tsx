2b1105d5a58cb882ce8576b95815c33fce9171fe
Nur Rochman <nurrochman954@gmail.com>
2025-02-06 09:04:01 +0700

    Update page admin

--- labmcb/app/admin/page.tsx
+++ labmcb/app/admin/page.tsx
@@ -1,38 +1,45 @@
 'use client'
 import React, { useState } from 'react';
 import ButtonDU from "@/components/TombolDU";
-import Filter from '@/components/Filter'; 
+import Filter from '@/components/Filter';
 import Overview from '@/components/Overview';
 import AdminTracking from '@/components/AdminTracking';
-
+import AdminTrackingSewaAlat from '@/components/AdminTrackingSewaAlat';
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import AdminTrackingSewaAlat from '@/components/AdminTrackingSewaAlat';
 
-const admin = () => {
+interface SearchResults {
+  [key: string]: number;
+}
 
+const AdminPage = () => {
   const [isFilterOpen, setIsFilterOpen] = useState(false);
-  const handleOpenFilter = () => setIsFilterOpen(true); 
-  const handleCloseFilter = () => setIsFilterOpen(false); 
+  const [searchResults, setSearchResults] = useState<SearchResults>({});
+
+  const handleOpenFilter = () => setIsFilterOpen(true);
+  const handleCloseFilter = () => setIsFilterOpen(false);
+
+  const handleSearchResults = (results: SearchResults) => {
+    setSearchResults(results);
+  };
 
   return (
     <>
       <Header />
-      <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-        
-        <Overview setIsFilterOpen={handleOpenFilter}/> 
-        {/* Mengatur margin kiri yang valid */}
-        
-          {/* Mengganti ml-100 dengan ml-10 atau nilai lainnya */}
-        
-          {isFilterOpen && <Filter onClose={handleCloseFilter} />}
-        <AdminTracking/>
-        <AdminTrackingSewaAlat/>
-        </div>
-      
+      <div className="flex flex-col min-h-screen">
+        <Overview setIsFilterOpen={handleOpenFilter} /> 
+        {isFilterOpen && (
+          <Filter 
+            onClose={handleCloseFilter} 
+            onSearch={handleSearchResults}
+          />
+        )}
+        <AdminTracking />
+        <AdminTrackingSewaAlat />
+      </div>
       <Footer />
     </>
   );
 };
 
-export default admin;
+export default AdminPage;
