4487643333e010617725c8bb99cc570107d44c13
Nur Rochman <nurrochman954@gmail.com>
2025-02-06 08:56:45 +0700

    Update ProfileBox fetch data from database and clerk

--- labmcb/components/ProfileBox.tsx
+++ labmcb/components/ProfileBox.tsx
@@ -1,15 +1,17 @@
+'use client';
 import React from 'react';
 
 interface ProfileBoxProps {
   text: string;
   imageUrl?: string;
-}
-
-const ProfileBox: React.FC<ProfileBoxProps> = ({ text, imageUrl }) => {
-  // Define the style for the gradient background
+ }
+ 
+ const ProfileBox: React.FC<ProfileBoxProps> = ({ text, imageUrl }) => {
+  const defaultImage = "/default-avatar.png";
+ 
   const gradientStyle: React.CSSProperties = {
     height: '120px',
-    marginBottom: '12px',
+    marginBottom: '12px', 
     background: 'linear-gradient(to right, #FDD5B2, #F27C70)',
     display: 'flex',
     alignItems: 'flex-end',
@@ -18,29 +20,36 @@ const ProfileBox: React.FC<ProfileBoxProps> = ({ text, imageUrl }) => {
     fontSize: '1.5rem',
     fontFamily: 'Lato, sans-serif',
   };
-
+ 
   const containerStyle: React.CSSProperties = {
     display: 'flex',
-    alignItems: 'center', // Mengatur alignment vertical ke tengah
-    paddingLeft: '30px', // Menambahkan padding kiri untuk menggeser ke kanan
+    alignItems: 'center',
+    paddingLeft: '30px',
   };
-
+ 
   const imageStyle: React.CSSProperties = {
-    width: '50px',
+    width: '50px', 
     height: '50px',
     borderRadius: '50%',
     marginRight: '15px',
     objectFit: 'cover',
   };
-
+ 
   return (
     <div style={gradientStyle}>
-      <div style={containerStyle}  className="w-[900px] justify-center">
-        {imageUrl && <img src={imageUrl} alt="Profile" style={imageStyle} />}
+      <div style={containerStyle} className="w-[900px] justify-center">
+        <img 
+          src={imageUrl || defaultImage} 
+          alt="Profile" 
+          style={imageStyle}
+          onError={(e) => {
+            e.currentTarget.src = defaultImage;
+          }}
+        />
         <h1>{text}</h1>
       </div>
     </div>
   );
-};
-
-export default ProfileBox;
+ };
+ 
+ export default ProfileBox;
