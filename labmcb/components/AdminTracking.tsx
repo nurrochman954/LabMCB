dcf55f39aee0d15bf72ae3391f567b21d83b2f9a
Nur Rochman <nurrochman954@gmail.com>
2025-02-06 08:34:04 +0700

    Update component AdminTracking

--- labmcb/components/AdminTracking.tsx
+++ labmcb/components/AdminTracking.tsx
@@ -3,15 +3,17 @@ import ButtonDU from './TombolDU';
 import PopUpNoSurat from '@/components/PopUpNoSurat';
 import DetailPermohonan from '@/components/DetailPermohonanPopup';
 import FileUploadAdmin from './FileUploadAdmin';
-import type { SampleTest, TestTimelineData, TrackingStatus } from '../types';
+import FileDownloadAdmin from './FileDownloadAdmin';
+import ComplaintAdmin from './PopUpKomplainAdmin';
+import type { SampleTest, TestTimelineData, AdminTrackingStatus } from '../types';
 
 const statusMapping = {
   isAdminReceived: 'RECEIVED',
   isSupervisorProcessing: 'REVIEWING',
   isSupervisorApproved: 'APPROVED',
   isSupervisorRejected: 'REJECTED',
-  isLaboranTesting: 'TESTING',
-  isPaymentNeeded: 'UNPAID',
+  isLaboranTested: 'TESTED',
+  isPaymentPaid: 'PAID',
   isFileUploaded: 'COMPLETED'
 } as const;
 
@@ -20,7 +22,7 @@ const AdminTracking: React.FC = () => {
   const [openTestId, setOpenTestId] = useState<number | null>(null);
   const [isDetailOpen, setIsDetailOpen] = useState(false);
   const [selectedDetailId, setSelectedDetailId] = useState<number | null>(null);
-  const [trackingStatus, setTrackingStatus] = useState<TrackingStatus>({});
+  const [trackingStatus, setTrackingStatus] = useState<AdminTrackingStatus>({});
   const [isPopupOpen, setIsPopupOpen] = useState(false);
   const [isRejectPopupOpen, setIsRejectPopupOpen] = useState(false);
   const [currentTestId, setCurrentTestId] = useState<number | null>(null);
@@ -34,7 +36,7 @@ const AdminTracking: React.FC = () => {
   const activeTests = sampleTests.filter(test => !isTestFinished(test.id));
   const completedTests = sampleTests.filter(test => isTestFinished(test.id));
 
-  
+
   useEffect(() => {
     const fetchInitialData = async () => {
       try {
@@ -42,7 +44,7 @@ const AdminTracking: React.FC = () => {
         const tests = await testsResponse.json();
         setSampleTests(tests);
 
-        const initialStatus: TrackingStatus = {};
+        const initialStatus: AdminTrackingStatus = {};
         for (const test of tests) {
           const timelineResponse = await fetch(`/api/sample-test/${test.id}/timeline`);
           const timelines = await timelineResponse.json();
@@ -52,9 +54,9 @@ const AdminTracking: React.FC = () => {
             isSupervisorProcessing: timelines.some((t: TestTimelineData) => t.testStatus === 'REVIEWING'),
             isSupervisorApproved: timelines.some((t: TestTimelineData) => t.testStatus === 'APPROVED'),
             isSupervisorRejected: timelines.some((t: TestTimelineData) => t.testStatus === 'REJECTED'),
-            isLaboranTesting: timelines.some((t: TestTimelineData) => t.testStatus === 'TESTING'),
-            isPaymentNeeded: timelines.some((t: TestTimelineData) => t.testStatus === 'UNPAID'),
-            isFileUploaded: timelines.some((t: TestTimelineData) => t.testStatus === 'COMPLETED')
+            isLaboranTested: timelines.some((t: TestTimelineData) => t.testStatus === 'TESTED'),
+            isPaymentPaid: timelines.some((t: TestTimelineData) => t.testStatus === 'PAID'),
+            isFileUploaded: timelines.some((t: TestTimelineData) => t.testStatus === 'COMPLETED'),
           };
         }
         setTrackingStatus(initialStatus);
@@ -79,13 +81,13 @@ const AdminTracking: React.FC = () => {
 
       setTrackingStatus(prev => ({
         ...prev,
-        [testId]: { 
-          ...prev[testId], 
+        [testId]: {
+          ...prev[testId],
           [status]: true,
           // Reset subsequent statuses if rejected
           ...(status === 'isSupervisorRejected' && {
-            isLaboranTesting: false,
-            isPaymentNeeded: false,
+            isLaboranTested: false,
+            isPaymentPaid: false,
             isFileUploaded: false
           })
         }
@@ -184,7 +186,7 @@ const AdminTracking: React.FC = () => {
   };
 
   const renderTestCard = (test: SampleTest) => (
-    <div key={test.id} className="bg-[#FAEBD7] p-5 rounded-lg border border-gray-300 w-[750px] mb-5">
+    <div key={test.id} className="bg-[#FAEBD7] p-5 rounded-lg border border-gray-300 w-[750px] mb-5 relative">
       <div className="flex justify-between items-center">
         <div className="flex flex-col">
           <h2 className="font-bold text-left text-[20px] cursor-pointer m-0" onClick={() => handleToggleAccordion(test.id)}>
@@ -200,84 +202,108 @@ const AdminTracking: React.FC = () => {
         </div>
         <img
           src="assets/ExternalLink.png"
-          alt="Open Detail"
+          alt="Open Detail" 
           className="h-8 w-10 mr-12 cursor-pointer"
           onClick={() => handleOpenDetail(test.id)}
         />
       </div>
-  
+   
       {openTestId === test.id && (
-        <div className="mt-4">
-          {renderStatusRow(test.id, 'isAdminReceived', 'Admin Laboratorium telah menerima surat pengantar dan sampel', false)}
-          {renderStatusRow(test.id, 'isSupervisorProcessing', 'Sedang diproses oleh Supervisor', false, 'isAdminReceived')}
-          {renderStatusRow(test.id, 'isSupervisorApproved', 'Supervisor menyetujui analisis sampel', false, 'isSupervisorProcessing')}
-          {renderStatusRow(test.id, 'isLaboranTesting', 'Laboran melakukan proses uji dan analisis pada sampel', false, 'isSupervisorApproved')}
-          {renderStatusRow(test.id, 'isPaymentNeeded', 'Customer perlu membayar', false, 'isLaboranTesting')}
-  
-          {trackingStatus[test.id]?.isLaboranTesting && (
-            <div className="mb-5">
-              <div className="flex gap-4">
-                <ButtonDU text="Download Bukti Pembayaran" variant="secondary" />
+        <>
+          <div className="mt-4">
+            {renderStatusRow(test.id, 'isAdminReceived', 'Admin Laboratorium telah menerima surat pengantar dan sampel', false)}
+            {renderStatusRow(test.id, 'isSupervisorProcessing', 'Sedang diproses oleh Supervisor', false, 'isAdminReceived')}
+            {renderStatusRow(test.id, 'isSupervisorApproved', 'Supervisor menyetujui analisis sampel', false, 'isSupervisorProcessing')}
+            {renderStatusRow(test.id, 'isLaboranTested', 'Laboran selesai melakukan proses uji dan analisis pada sampel', false, 'isSupervisorApproved')}
+            {renderStatusRow(test.id, 'isPaymentPaid', 'Customer perlu membayar', false, 'isLaboranTested')}
+   
+            {trackingStatus[test.id]?.isLaboranTested && (
+              <div className="mb-5">
+                <div className="flex gap-4">
+                  <FileDownloadAdmin
+                    id={test.id}
+                    buttonText="Download Bukti Pembayaran" 
+                    fileType="paymentProof"
+                    formType="sample-test"
+                  />
+                  <FileUploadAdmin
+                    id={test.id}
+                    buttonText="Unggah Invoice"
+                    uploadPreset="labmcbpreset"
+                    folder="invoice"
+                    fileType="invoice"
+                    formType="sample-test"
+                  />
+                </div>
+              </div>
+            )}
+   
+            {renderStatusRow(test.id, 'isFileUploaded', 'Admin Laboratorium telah mengunggah file hasil uji', false, 'isPaymentPaid')}
+   
+            {trackingStatus[test.id]?.isPaymentPaid && (
+              <div className="mb-5">
                 <FileUploadAdmin
-                  testId={test.id}
-                  buttonText="Unggah Invoice"
+                  id={test.id}
+                  buttonText="Upload File Uji"
                   uploadPreset="labmcbpreset"
-                  folder="invoice"
-                  fileType="invoice"
+                  folder="test_results"
+                  fileType="result"
+                  formType="sample-test"
                 />
               </div>
-            </div>
-          )}
-  
-          {renderStatusRow(test.id, 'isFileUploaded', 'Admin Laboratorium telah mengunggah file hasil uji', false, 'isPaymentNeeded')}
-  
-          {trackingStatus[test.id]?.isPaymentNeeded && (
-            <div className="mb-5">
-              <FileUploadAdmin
-                testId={test.id}
-                buttonText="Upload File Uji"
-                uploadPreset="labmcbpreset"
-                folder="test_results"
-                fileType="result"
-              />
-            </div>
-          )}
-        </div>
+            )}
+          </div>
+          
+          <div className="absolute bottom-4 right-4">
+            <ComplaintAdmin
+              formId={test.id}
+              formType="sample-test"
+            />
+          </div>
+        </>
       )}
     </div>
-  );
-  
+   );
+   
   return (
     <div className="flex justify-center items-center">
       <div className="w-3/5 px-6 py-4">
         <h2 className="text-2xl font-bold mb-4">Permohonan Analisis</h2>
-        
+
         {activeTests.length > 0 && (
           <>
             <h3 className="text-xl mb-4">Terbaru</h3>
-            {activeTests.map(renderTestCard)}
+            {activeTests.map((test) => (
+              <div key={test.id}>  {/* Tambahkan key di sini */}
+                {renderTestCard(test)}
+              </div>
+            ))}
           </>
         )}
-  
+
         {completedTests.length > 0 && (
           <>
             <h3 className="text-xl mb-4">Selesai</h3>
-            {completedTests.map(renderTestCard)}
+            {completedTests.map((test) => (
+              <div key={test.id}>  {/* Tambahkan key di sini */}
+                {renderTestCard(test)}
+              </div>
+            ))}
           </>
         )}
-  
+
         {sampleTests.length === 0 && (
           <p className="text-gray-500 text-center">Tidak ada permohonan analisis</p>
         )}
       </div>
-  
+
       {isDetailOpen && selectedDetailId && (
         <DetailPermohonan
           id={selectedDetailId}
           onClose={() => setIsDetailOpen(false)}
         />
       )}
-  
+
       {isPopupOpen && currentTestId && (
         <PopUpNoSurat
           onClose={() => setIsPopupOpen(false)}
--- labmcb/types.ts
+++ labmcb/types.ts
@@ -1,44 +1,58 @@
 // types.ts
 import { TestStatus } from '@prisma/client';
 export interface SampleTest {
-    id: number;
-    testName: string;
-    sampleRequestNumber: string;
-    status: string;
+  id: number;
+  testName: string;
+  sampleRequestNumber: string;
+  status: string;
+}
+
+export interface TestTimelineData {
+  id: number;
+  testStatus: TestStatus;
+  testTimelineCreatedAt: Date;
+}
+
+// types.ts
+export interface BaseTrackingStatus {
+  isAdminReceived: boolean;
+  isSupervisorProcessing: boolean;
+  isSupervisorApproved: boolean;
+  isSupervisorRejected: boolean;
+  isLaboranTested: boolean;
+  isPaymentPaid: boolean;
+  isFileUploaded: boolean;
+}
+
+interface TrackingStatus {
+
+  createdAt?: Date;
+}
+
+export interface AdminTrackingStatus {
+  [key: number]: BaseTrackingStatus;
+}
+
+export interface UserTrackingStatus {
+  [key: number]: BaseTrackingStatus & {
+    createdAt: Date;
   }
-  
-  export interface TestTimelineData {
-    id: number;
-    testStatus: TestStatus;
-    testTimelineCreatedAt: Date;
-  }
-  
-  export interface TrackingStatus {
-    [key: number]: {
-      isAdminReceived: boolean;
-      isSupervisorProcessing: boolean;
-      isSupervisorApproved: boolean;
-      isSupervisorRejected: boolean;
-      isLaboranTesting: boolean;
-      isPaymentNeeded: boolean;
-      isFileUploaded: boolean;
-    }
-  }
-  
-  export interface CloudinaryUploadWidgetInfo {
-    url: string;
-    resource_type: string;
-    format: string;
-    secure_url: string;
-    public_id: string;
-    original_filename: string;
-  }
-  
-  export interface UploadedFile {
-    url: string;
-    downloadUrl: string;
-    type: 'image' | 'pdf';
-    filename?: string;
-    format: string;
-    public_id: string;
-  }
+}
+
+export interface CloudinaryUploadWidgetInfo {
+  url: string;
+  resource_type: string;
+  format: string;
+  secure_url: string;
+  public_id: string;
+  original_filename: string;
+}
+
+export interface UploadedFile {
+  url: string;
+  downloadUrl: string;
+  type: 'image' | 'pdf';
+  filename?: string;
+  format: string;
+  public_id: string;
+}
